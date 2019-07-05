2c08cbd114a6e4bf763e165771756f5540ac7643
Kyle Koder <silvertallon@gmail.com>
2019-07-04 21:19:03 -0700

    Working on it, almost there...

--- src/main/java/com/fantasticsource/loginhpfix/LoginHPFix.java
+++ src/main/java/com/fantasticsource/loginhpfix/LoginHPFix.java
@@ -1,14 +1,19 @@
 package com.fantasticsource.loginhpfix;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
-@Mod(modid = LoginHPFix.MODID, name = LoginHPFix.NAME, version = LoginHPFix.VERSION)
+import java.util.Set;
+
+@Mod(modid = LoginHPFix.MODID, name = LoginHPFix.NAME, version = LoginHPFix.VERSION, acceptableRemoteVersions = "*")
 public class LoginHPFix
 {
     public static final String MODID = "loginhpfix";
@@ -25,5 +30,39 @@ public class LoginHPFix
     public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
     {
         if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void playerLogon(PlayerEvent.PlayerLoggedInEvent event)
+    {
+        //TODO need to do this a bit later, in a more flexible way...maybe on max hp change within first x ticks if there's an event for that, otherwise just check after x ticks
+        EntityPlayer player = event.player;
+        Set<String> strings = player.getTags();
+        for (String s : strings.toArray(new String[0]))
+        {
+            if (s.contains("loginhp"))
+            {
+                player.setHealth(player.getMaxHealth() * Float.parseFloat(s.replace("loginhp", "")));
+                strings.remove(s);
+                break;
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void playerLogoff(PlayerEvent.PlayerLoggedOutEvent event)
+    {
+        EntityPlayer player = event.player;
+        Set<String> strings = player.getTags();
+        for (String s : strings.toArray(new String[0]))
+        {
+            if (s.contains("loginhp"))
+            {
+                strings.remove(s);
+                break;
+            }
+        }
+        strings.add("loginhp" + String.format("%.2f", player.getHealth() / player.getMaxHealth()));
+        System.out.println("loginhp" + String.format("%.2f", player.getHealth() / player.getMaxHealth()));
     }
 }
