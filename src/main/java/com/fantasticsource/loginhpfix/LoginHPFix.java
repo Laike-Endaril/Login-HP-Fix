e598a6164ca078abbce8334ed41c2732a79f8688
Kyle Koder <silvertallon@gmail.com>
2019-07-04 22:03:18 -0700

    1.12.2.001

--- src/main/java/com/fantasticsource/loginhpfix/LoginHPFix.java
+++ src/main/java/com/fantasticsource/loginhpfix/LoginHPFix.java
@@ -20,7 +20,7 @@ public class LoginHPFix
 {
     public static final String MODID = "loginhpfix";
     public static final String NAME = "Login HP Fix";
-    public static final String VERSION = "1.12.2.000";
+    public static final String VERSION = "1.12.2.001";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
